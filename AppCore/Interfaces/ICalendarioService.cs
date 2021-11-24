9711a4d448a89ab3aa7848676bc90fe390e920fb
Steffania Salgado <86125323+SalgadoSteffania@users.noreply.github.com>
2021-11-24 11:03:17 -0600

    Add files via upload

---
+++ AppCore/Interfaces/ICalendarioService.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCore.Interfaces
+{
+    public interface ICalendarioService : IService<Calendario>
+    {
+        int GetLastIdCuota();
+    }
+}
