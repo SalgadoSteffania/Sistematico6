e0e4fe207e213fc6ae1b73a08d1936d7268312d1
Steffania Salgado <86125323+SalgadoSteffania@users.noreply.github.com>
2021-11-24 11:02:01 -0600

    Add files via upload

---
+++ Domain/Calendario.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Enums;
+using System;
+
+namespace Domain
+{
+    public class Calendario
+    {
+        public int Id { get; set; }
+        public Tipo Tipo { get; set; }
+        public Estado Estado { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public DateTime FechaPaga { get; set; }
+        public decimal Monto { get; set; }
+        public int Terminos { get; set; }
+        public decimal Tasa { get; set; }
+        public decimal Principal { get; set; }
+        public decimal Interest { get; set; }
+        public decimal Impuestos { get; set; }
+        public decimal MontoTotal { get; set; }
+
+
+
+
+
+
+    }
+}
---
+++ Domain/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Add(Calendario cln);
+        List<T> GetAll();
+        int GetLastIdCuota();
+    }
+}
